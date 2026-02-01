1491de7dba4b479ccdf194f0a012eefe987bb793
Amartya Ghosh <amartyaghosh48@gmail.com>
2026-02-01 20:32:04 +0530

    auth & stream sync

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -1,6 +1,7 @@
 import { Inngest } from "inngest";
 import { connectDB } from "./db.js";
 import User from "../models/User.js";
+import { deleteStreamUser, upsertStreamUser } from "./stream.js";
 
 export const inngest = new Inngest({ id: "interview-video-calling-app" });
 
@@ -21,20 +22,27 @@ const syncUser = inngest.createFunction(
     };
 
     await User.create(newUser);
+
+    await upsertStreamUser({
+      id: newUser.clerkId.toString(),
+      name: newUser.name,
+      image: newUser.profileImage,
+    });
   },
 );
 
 const deleteUserFromDB = inngest.createFunction(
-    {id:"delete-user-from-db"},
-    {event:"clerk/user.deleted"},
-    async ({event}) => {
-        await connectDB()
+  { id: "delete-user-from-db" },
+  { event: "clerk/user.deleted" },
+  async ({ event }) => {
+    await connectDB();
 
-        const {id} = event.data;
+    const { id } = event.data;
 
-        await User.deleteOne({clerkId:id})
+    await User.deleteOne({ clerkId: id });
 
-    }
-)
+    await deleteStreamUser(id.toString());
+  },
+);
 
-export const functions = [syncUser,deleteUserFromDB];
+export const functions = [syncUser, deleteUserFromDB];
