4f7eb05fe5d9ff02ead6d8d9d3435d705ae141d9
Amartya Ghosh <amartyaghosh48@gmail.com>
2026-02-13 20:03:55 +0530

    problems page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,9 @@
 import { SignedOut, SignedIn, SignInButton, SignOutButton, UserButton, useUser } from "@clerk/clerk-react";
 import { Navigate, Route, Routes } from "react-router";
 import HomePage from "./pages/HomePage";
-import ProblemPage from "./pages/ProblemPage";
 import { Toaster } from "react-hot-toast";
 import DashboardPage from "./pages/DashboardPage";
+import ProblemsPage from "./pages/ProblemsPage";
 
 function App() {
 
@@ -15,9 +15,12 @@ function App() {
   return (
     <>
     <Routes>
-      <Route path="/" element={!isSignedIn ? <HomePage /> : <Navigate to={"/dashboard"} />} />
-      <Route path="/dashboard" element={isSignedIn ? <DashboardPage /> : <Navigate to={"/"} />} />
-      <Route path="/problems" element={isSignedIn ? <ProblemPage /> : <Navigate to={"/"} />} />
+      {/* <Route path="/" element={!isSignedIn ? <HomePage /> : <Navigate to={"/dashboard"} />} /> */}
+      <Route path="/" element={<HomePage />} />
+      {/* <Route path="/dashboard" element={isSignedIn ? <DashboardPage /> : <Navigate to={"/"} />} /> */}
+      <Route path="/dashboard" element={<DashboardPage />} />
+      <Route path="/problems" element={isSignedIn ? <ProblemsPage /> : <Navigate to={"/"} />} />
+      {/* <Route path="/problems" element={<ProblemsPage /> } /> */}
     </Routes>
     <Toaster />
     </>
