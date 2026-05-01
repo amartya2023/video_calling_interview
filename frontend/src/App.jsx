0957a4d9df19f3b9b0c1cd19c882e931fbbe97c3
Amartya Ghosh <amartyaghosh48@gmail.com>
2026-05-01 15:52:54 +0530

    problem detail page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import HomePage from "./pages/HomePage";
 import { Toaster } from "react-hot-toast";
 import DashboardPage from "./pages/DashboardPage";
 import ProblemsPage from "./pages/ProblemsPage";
+import ProblemPage from "./pages/ProblemPage";
 
 function App() {
 
@@ -21,8 +22,10 @@ function App() {
       <Route path="/dashboard" element={<DashboardPage />} />
       <Route path="/problems" element={isSignedIn ? <ProblemsPage /> : <Navigate to={"/"} />} />
       {/* <Route path="/problems" element={<ProblemsPage /> } /> */}
+      <Route path="/problem/:id" element={isSignedIn ? <ProblemPage /> : <Navigate to={"/"} />} />
     </Routes>
-    <Toaster />
+
+    <Toaster toastOptions={{duration: 3000}} />
     </>
   );
 }
