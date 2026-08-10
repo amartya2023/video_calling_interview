5508bd51e891f407c1ff048fffec5383dabab0e7
amartya2023 <amartyaghosh48@gmail.com>
2026-08-10 12:27:04 +0530

    session page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,49 @@
-import { SignedOut, SignedIn, SignInButton, SignOutButton, UserButton, useUser } from "@clerk/clerk-react";
-import { Navigate, Route, Routes } from "react-router";
-import HomePage from "./pages/HomePage";
+import {
+  useUser
+} from "@clerk/clerk-react";
 import { Toaster } from "react-hot-toast";
+import { Navigate, Route, Routes } from "react-router";
 import DashboardPage from "./pages/DashboardPage";
-import ProblemsPage from "./pages/ProblemsPage";
+import HomePage from "./pages/HomePage";
 import ProblemPage from "./pages/ProblemPage";
+import ProblemsPage from "./pages/ProblemsPage";
+import SessionPage from "./pages/SessionPage";
 
 function App() {
-
-  const {isSignedIn, isLoaded} = useUser();
+  const { isSignedIn, isLoaded } = useUser();
 
   // this will get rid of the flickering effect
-  if(!isLoaded) return null;
+  if (!isLoaded) return null;
 
   return (
     <>
-    <Routes>
-      {/* <Route path="/" element={!isSignedIn ? <HomePage /> : <Navigate to={"/dashboard"} />} /> */}
-      <Route path="/" element={<HomePage />} />
-      {/* <Route path="/dashboard" element={isSignedIn ? <DashboardPage /> : <Navigate to={"/"} />} /> */}
-      <Route path="/dashboard" element={<DashboardPage />} />
-      <Route path="/problems" element={isSignedIn ? <ProblemsPage /> : <Navigate to={"/"} />} />
-      {/* <Route path="/problems" element={<ProblemsPage /> } /> */}
-      <Route path="/problem/:id" element={isSignedIn ? <ProblemPage /> : <Navigate to={"/"} />} />
-    </Routes>
+      <Routes>
+        <Route
+          path="/"
+          element={!isSignedIn ? <HomePage /> : <Navigate to={"/dashboard"} />}
+        />
+        {/* <Route path="/" element={<HomePage />} /> */}
+        <Route
+          path="/dashboard"
+          element={isSignedIn ? <DashboardPage /> : <Navigate to={"/"} />}
+        />
+        {/* <Route path="/dashboard" element={<DashboardPage />} /> */}
+        <Route
+          path="/problems"
+          element={isSignedIn ? <ProblemsPage /> : <Navigate to={"/"} />}
+        />
+        {/* <Route path="/problems" element={<ProblemsPage /> } /> */}
+        <Route
+          path="/problem/:id"
+          element={isSignedIn ? <ProblemPage /> : <Navigate to={"/"} />}
+        />
+        <Route
+          path="/session/:id"
+          element={isSignedIn ? <SessionPage /> : <Navigate to={"/"} />}
+        />
+      </Routes>
 
-    <Toaster toastOptions={{duration: 3000}} />
+      <Toaster toastOptions={{ duration: 3000 }} />
     </>
   );
 }
